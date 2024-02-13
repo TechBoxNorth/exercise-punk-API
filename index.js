3bb828bfda050ee21f4cc0f53f0ca292899b7968
TechBoxNorth <techboxnorth@gmail.com>
2024-02-13 19:34:39 +0100

    switching to beer info page that has all required info now works

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ container.addEventListener('click', (e) => {
     if(e.target.classList.contains('show-random-btn')){
         getRandomBeer();
     }
+
+    if(e.target.classList.contains('see-more-btn')){
+        document.querySelector('.section-random-beer').style.display = 'none';
+        document.querySelector('.section-beer-info').style.display = 'inherit';
+    }
 });
 
 async function randomBeer(){
@@ -18,9 +23,80 @@ async function randomBeer(){
 
 function getRandomBeer(){
     randomBeer().then((data) => {
-        document.querySelector('.beer-name').innerText = data[0].name;
-        document.querySelector('.b-img').src = data[0].image_url;
+        console.log(data[0].description);
+        document.querySelector('.beer-name').innerText = setBeerName(data);
+        document.querySelector('.b-img').src = setBeerImage(data);
+        document.querySelector('.info-img').src = setBeerImage(data);
+        setHops(data);
+        setMalts(data);
+        setYeast(data);
+        setDescription(data);
+        setAbv(data);
+        setFoodPairing(data);
+        setBrewersTips(data);
     })
+}
+
+function setBeerName(data){
+    return data[0].name;
+}
+
+function setDescription(data){
+    const description = document.querySelector('.description');
+    let p = document.createElement('p');
+    p.innerText = data[0].description;
+    description.appendChild(p);
+}
+
+function setBeerImage(data){
+    return data[0].image_url;
+}
+
+function setHops(data){
+    const hops = document.querySelector('.hops');
+    for(let i = 0; i < data[0].ingredients.hops.length; i++){
+        console.log(data[0].ingredients.hops[i].name);
+        let p = document.createElement('p');
+        p.innerText = data[0].ingredients.hops[i].name;
+        hops.appendChild(p);
+    }
+}
+
+function setMalts(data){
+    const malts = document.querySelector('.malts');
+    for(let i = 0; i < data[0].ingredients.malt.length; i++){
+        let p = document.createElement('p');
+        p.innerText = data[0].ingredients.malt[i].name;
+        malts.appendChild(p);
+    }
+}
+
+function setYeast(data){
+    const yeast = document.querySelector('.yeast');
+    let p = document.createElement('p');
+    p.innerText = data[0].ingredients.yeast;
+    yeast.appendChild(p);
+}
+
+function setAbv(data){
+    const abv = document.querySelector('.abv');
+    let p = document.createElement('p');
+    p.innerText = data[0].abv;
+    abv.appendChild(p);
+}
+
+function setFoodPairing(data){
+    const foodPairing = document.querySelector('.food-pairing');
+    let p = document.createElement('p');
+    p.innerText = data[0].food_pairing;
+    foodPairing.appendChild(p);
+}
+
+function setBrewersTips(data){
+    const brewersTips = document.querySelector('.brewers-tips');
+    let p = document.createElement('p');
+    p.innerText = data[0].brewers_tips;
+    brewersTips.appendChild(p);
 }
 
 getRandomBeer();
